5b54afcd3348649eca1be19dabb13ca0e0fea201
NorthDakota11 <northdakota11@github.com>
2026-05-19 17:45:39 +0000

    Clean CMakeLists and disable test_ocp

--- Patterns/SOLID/Program.cpp
+++ Patterns/SOLID/Program.cpp
@@ -4,7 +4,7 @@
 
 // function prototypes
 extern void test_srp();
-extern void test_ocp();
+// extern void // test_ocp();
 extern void test_lsp();
 extern void test_isp();
 extern void test_dip();
@@ -12,7 +12,7 @@ extern void test_dip();
 int main()
 {
     test_srp();
-    test_ocp();
+    // test_ocp();
     test_lsp();
     test_isp();
     test_dip();
